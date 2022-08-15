a50b276f8cc98727416911cd346554d8ddc89756
andrii.leonets <aleonets@gmail.com>
2022-08-15 17:11:23 +0300

    move scenario to folder scenarios

--- src/main/java/io/airbyte/testingtool/scenario/config/ScenarioConfigService.java
+++ src/main/java/io/airbyte/testingtool/scenario/config/ScenarioConfigService.java
@@ -8,7 +8,7 @@ import java.nio.file.Path;
 public class ScenarioConfigService {
 
   public static ScenarioConfig getConfig(String[] args) throws IOException {
-    final String fullConfigAsString = Files.readString(Path.of("src/main/resources/poc-scenario.json"));
+    final String fullConfigAsString = Files.readString(Path.of("src/main/resources/scenarios/poc-scenario.json"));
     return Jsons.deserialize(fullConfigAsString, ScenarioConfig.class);
   }
 
