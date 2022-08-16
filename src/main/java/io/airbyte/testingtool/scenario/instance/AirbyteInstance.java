b96c20b9673a0ef1508eab41c4158680ff1ef190
mtuhai <mtuhai@talend.com>
2022-08-16 12:47:26 +0300

    15580: merge

--- src/main/java/io/airbyte/testingtool/scenario/instance/AirbyteInstance.java
+++ src/main/java/io/airbyte/testingtool/scenario/instance/AirbyteInstance.java
@@ -7,10 +7,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import lombok.Setter;
 
 public class AirbyteInstance extends InstanceWithCredentials {
   private static final Logger LOGGER = LoggerFactory.getLogger(AirbyteInstance.class);
