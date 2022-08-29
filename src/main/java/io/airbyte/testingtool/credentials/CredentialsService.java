c9077f3abc6e0667aa2bb2b6e790fb50d32f60cf
Andrii Korotkov <andrii.h.korotkov@globallogic.com>
2022-08-29 16:51:19 +0300

    updated method for read secerts from Secret Manager

--- src/main/java/io/airbyte/testingtool/credentials/CredentialsService.java
+++ src/main/java/io/airbyte/testingtool/credentials/CredentialsService.java
@@ -1,7 +1,5 @@
 package io.airbyte.testingtool.credentials;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import com.google.cloud.secretmanager.v1.SecretName;
@@ -9,6 +7,9 @@ import com.google.cloud.secretmanager.v1.SecretVersion;
 import io.airbyte.testingtool.argument_parser.Command;
 import io.airbyte.testingtool.json.Jsons;
 import io.airbyte.testingtool.scenario.config.CredentialConfig;
+import io.airbyte.testingtool.scenario.config.ServiceAccountConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -20,8 +21,10 @@ import java.util.stream.StreamSupport;
 
 public class CredentialsService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(CredentialsService.class);
   private static final String LOCAL_SECRET_FOLDER = "secrets/";
-  private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  private static ServiceAccountConfig serviceAccountConfig;
 
   public static Map<String, CredentialConfig> getCredentials(final Command runCommand, final Map<String, String> args) throws IOException {
     return switch (runCommand) {
@@ -31,8 +34,13 @@ public class CredentialsService {
     };
   }
 
-  public static Map<String, CredentialConfig> getCredentialsFromSecretService(final Map<String, String> args) {
-    throw new RuntimeException("Secret service is not integrated yet!");
+  public static Map<String, CredentialConfig> getCredentialsFromSecretService(final Map<String, String> args) throws IOException {
+    Map<String, CredentialConfig> credentials = new HashMap<>();
+    for (Entry<String, String> nameCredentialsMap : args.entrySet()) {
+      credentials.put(nameCredentialsMap.getKey(), readSecretManagerCredential(nameCredentialsMap.getValue()));
+    }
+
+    return credentials;
   }
 
   public static Map<String, CredentialConfig> getCredentialsFromLocalSecrets(final Map<String, String> args) throws IOException {
@@ -53,27 +61,31 @@ public class CredentialsService {
     }
   }
 
-  public static JsonNode getSecrets(String secretId) throws IOException {
-    var projectId = getProjectIdForSecretManager();
+  private static CredentialConfig readSecretManagerCredential(final String secretName) throws IOException {
+    var projectName = serviceAccountConfig.getProjectId();
     try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
-      SecretName secretNameForGetActiveVersion = SecretName.of(projectId, secretId);
+      SecretName secretNameForGetActiveVersion = SecretName.of(projectName, secretName);
       SecretManagerServiceClient.ListSecretVersionsPagedResponse versionList = client.listSecretVersions(secretNameForGetActiveVersion);
       var optionSecretWithVersion = StreamSupport.stream(versionList.iterateAll().spliterator(), false)
               .filter(secretVersion -> secretVersion.getState().equals(SecretVersion.State.ENABLED))
               .findFirst();
 
       if (optionSecretWithVersion.isEmpty()) {
-        throw new RuntimeException(String.format("Driver could not find active version of \"%s\" secret.", secretId));
+        throw new RuntimeException(String.format("Driver could not find active version of \"%s\" secret.", secretName));
       }
       var secretNameWithVersion = optionSecretWithVersion.get().getName();
       AccessSecretVersionResponse response = client.accessSecretVersion(secretNameWithVersion);
-      return MAPPER.readTree(response.getPayload().getData().toStringUtf8());
+      return Jsons.deserialize(response.getPayload().getData().toStringUtf8(), CredentialConfig.class);
     }
   }
 
-  private static String getProjectIdForSecretManager() throws IOException {
-    final String secrets = Files.readString(Path.of("secrets/config.json"));
-    var secretsNode = Jsons.deserialize(secrets);
-    return secretsNode.get("projectId").asText();
+  static {
+    try {
+      String fullConfigAsString = Files.readString(Path.of("/secrets/service_account_credentials.json"));
+      serviceAccountConfig = Jsons.deserialize(fullConfigAsString, ServiceAccountConfig.class);
+    } catch (IOException e) {
+      LOGGER.error("Fail to parse \"service_account_credentials.json\" config file!");
+    }
   }
+
 }
