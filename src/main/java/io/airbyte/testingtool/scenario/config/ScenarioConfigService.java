0d08e58f39d48c47cacc77c6576d079acd4c1fcc
mkhokh <mariia.khokh@globallogic.com>
2022-09-08 12:42:20 +0300

    added changes to read scenario files within jar file and some changes into docker file

--- src/main/java/io/airbyte/testingtool/scenario/config/ScenarioConfigService.java
+++ src/main/java/io/airbyte/testingtool/scenario/config/ScenarioConfigService.java
@@ -3,9 +3,14 @@ package io.airbyte.testingtool.scenario.config;
 import io.airbyte.testingtool.json.Jsons;
 import io.airbyte.testingtool.scenario.validator.ValidationService;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.Getter;
@@ -15,6 +20,7 @@ import org.slf4j.LoggerFactory;
 public class ScenarioConfigService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ScenarioConfigService.class);
+  private static final String SCENARIO_PATH = "/scenarios";
 
   @Getter
   private final static Map<String, ScenarioConfig> scenarioConfigs;
@@ -34,8 +40,7 @@ public class ScenarioConfigService {
 
   static {
     scenarioConfigs = new HashMap<>();
-    Path ScenarioPath =  Paths.get(ScenarioConfigService.class.getClassLoader().getResource("scenarios/").getPath());
-    try (var paths = Files.walk(ScenarioPath)) {
+    try (var paths = Files.walk(getPath())) {
       paths.filter(Files::isRegularFile).forEach(path -> {
         try {
           String fullConfigAsString = Files.readString(path);
@@ -45,8 +50,18 @@ public class ScenarioConfigService {
           LOGGER.error("Fail to parse scenario config file : {}!", path);
         }
       });
-    } catch (IOException e) {
+    } catch (Exception e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  private static Path getPath() throws IOException, URISyntaxException {
+    URI uri = ScenarioConfigService.class.getResource(SCENARIO_PATH).toURI();
+    if ("jar".equals(uri.getScheme())) {
+      FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.emptyMap());
+      return fileSystem.getPath(SCENARIO_PATH);
+    } else {
+      return Paths.get(uri);
     }
   }
 
