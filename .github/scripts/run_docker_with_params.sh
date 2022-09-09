1262df451bec83aaae371ebed128c707d3f57cff
andriikorotkov <88329385+andriikorotkov@users.noreply.github.com>
2022-09-09 10:54:32 +0300

    Update run_docker_with_params.sh

--- .github/scripts/run_docker_with_params.sh
+++ .github/scripts/run_docker_with_params.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker_arg="$1"
+docker_arg="$1" 
 echo "Docker arg = $docker_arg"
 #docker build -t testingtool .
 #docker run -it  testingtool:latest /help-full name="$docker_arg"
