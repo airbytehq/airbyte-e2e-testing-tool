66fdcbb8db90445bde7df6bc07aad9280d78c278
Andrii Korotkov <andrii.h.korotkov@globallogic.com>
2022-09-13 14:24:18 +0300

    update workflow

--- .github/scripts/run_docker_with_params.sh
+++ .github/scripts/run_docker_with_params.sh
@@ -3,5 +3,6 @@
 docker_arg="$1"
 echo "Docker arg = $docker_arg"
 docker build -t testingtool .
-com='docker run testingtool:latest $docker_arg'
+com="docker run testingtool:latest $docker_arg"
+echo "===> $com"
 eval "$com"
