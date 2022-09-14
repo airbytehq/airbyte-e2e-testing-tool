38027e820b089ea344942f40ca759e5e8c76c38a
mkhokh <mariia.khokh@globallogic.com>
2022-09-14 16:05:25 +0300

    change github action to run from current branch

--- .github/scripts/run_docker_with_params.sh
+++ .github/scripts/run_docker_with_params.sh
@@ -3,5 +3,5 @@
 docker_arg="$1"
 echo "Docker testing tool args = $docker_arg"
 docker build -t testingtool .
-com="docker run testingtool:latest $docker_arg"
+com="docker run -v $(pwd)/secrets:/app/lib/secrets testingtool:latest $docker_arg"
 eval "$com"
